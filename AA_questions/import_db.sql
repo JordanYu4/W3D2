2f46d8c8500b128ac975f7e1e6f09a2a6f5eb4e8
JordanYu4 <jordanyu44@gmail.com>
2018-07-03 15:06:46 -0700

    Complete Easy Mode

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
@@ -9,13 +15,16 @@ CREATE TABLE users (
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
-  body TEXT NOT NULL
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 CREATE TABLE question_follows (
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
-  
+
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
@@ -26,11 +35,11 @@ CREATE TABLE replies (
   parent_reply INTEGER,
   user INTEGER NOT NULL,
   body_text NOT NULL,
-  
+
   FOREIGN KEY (subject_question) REFERENCES questions(id),
   FOREIGN KEY (parent_reply) REFERENCES replies(id),
   FOREIGN KEY (user) REFERENCES users(id)
-  
+
 );
 
 CREATE TABLE question_likes (
@@ -39,7 +48,7 @@ CREATE TABLE question_likes (
 
   FOREIGN KEY (question_liked) REFERENCES questions(id),
   FOREIGN KEY (user_liking) REFERENCES users(id)
-  
+
 );
 
 INSERT INTO
@@ -47,40 +56,51 @@ INSERT INTO
 VALUES
   ('Jordan', 'Yu'),
   ('Eric', 'Tran'),
-  ('Bob', 'Builder');
-  
+  ('Bob', 'Builder'),
+  ('Mom', 'Tran'),
+  ('Amanda', 'Yu');
+
 INSERT INTO
-  questions (title, body)
+  questions (title, body, author_id)
 VALUES
-  ('Help', 'I''ve fallen and I can''t get up'),
-  ('Baby', 'Whose baby is this?');
-  
+  ('Help', 'I''ve fallen and I can''t get up', 2),
+  ('Baby', 'Whose baby is this?', 1),
+  ('Job', 'I need employment?', 4);
+
 INSERT INTO
   question_follows (user_id, question_id)
 VALUES
+  (
+    (SELECT id FROM users WHERE fname = 'Bob'),
+    (SELECT id FROM questions WHERE title = 'Baby')
+  ),
   (
     (SELECT id FROM users WHERE fname = 'Jordan'),
     (SELECT id FROM questions WHERE title = 'Help')
   ),
   (
     (SELECT id FROM users WHERE fname = 'Eric'),
-    (SELECT id FROM questions WHERE title = 'Baby')
+    (SELECT id FROM questions WHERE title = 'Job')
   );
-  
+
 INSERT INTO
   replies (subject_question, parent_reply, user, body_text)
 VALUES
   (
     (SELECT id FROM questions WHERE title = 'Help'),
     NULL,
-    (SELECT id FROM users WHERE fname = 'Eric'),
+    (SELECT id FROM users WHERE fname = 'Bob'),
     'Sorry, I''m busy.'
   );
-  
+
 INSERT INTO
   question_likes (question_liked, user_liking)
 VALUES
   (
     (SELECT id FROM questions WHERE title = 'Help'),
-    (SELECT id FROM users WHERE fname = 'Bob')
+    (SELECT id FROM users WHERE fname = 'Jordan')
+  ),
+  (
+    (SELECT id FROM questions WHERE title = 'Help'),
+    (SELECT id FROM users WHERE fname = 'Amanda')
   );
--- AA_questions/questions_database.rb
+++ AA_questions/questions_database.rb
@@ -23,16 +23,34 @@ class User
     user.length == 0 ? nil : User.new(user.first)
   end
   
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT *
+      FROM users
+      WHERE fname = ? AND lname = ?
+    SQL
+    User.new(user.first)
+  end
+  
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
   end
   
+  def authored_questions
+    Question.find_by_author_id(id)
+  end
+  
+  def authored_replies
+    Reply.find_by_user_id(id)
+  end
+  
 end
 
 class Question
   attr_accessor :id, :title, :body
+  attr_reader :author_id
   
   def self.find_by_id(question_id)
     question = QuestionsDatabase.instance.execute(<<-SQL, question_id)
@@ -43,19 +61,45 @@ class Question
     question.length == 0 ? nil : Question.new(question.first)
   end
   
+  def self.find_by_author_id(author_id)
+    author_questions = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT *
+      FROM questions
+      WHERE author_id = ?
+    SQL
+    author_questions.map{ |author_question| Question.new(author_question) }
+  end
+  
   def initialize(options)
     @id = options['id']
     @title = options['title']
     @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def replies
+    Reply.find_by_question_id(id)
   end
   
 end
 
 class QuestionFollow
+  
+  def self.find_by_id(question_id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT user_id
+      FROM questions_follows
+      WHERE question_id = ?
+    SQL
+    question
+  end
+  
 end
 
 class Reply
-  attr_accessor :id, :subject_question, :parent_reply, :user, :body_text
+  attr_accessor :id, :body_text
+  attr_reader :user, :subject_question, :parent_reply
+  
   def self.find_by_id(reply_id)
     reply = QuestionsDatabase.instance.execute(<<-SQL, reply_id)
       SELECT *
@@ -63,6 +107,24 @@ class Reply
       WHERE id = ?
     SQL
     reply.length == 0 ? nil : Reply.new(reply.first)
+  end
+  
+  def self.find_by_user_id(user_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT *
+      FROM replies
+      WHERE user = ?
+    SQL
+    replies.map{|reply| Reply.new(reply)}
+  end
+  
+  def self.find_by_question_id(question_id)
+    replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT *
+      FROM replies
+      WHERE subject_question = ?
+    SQL
+    replies.map{|reply| Reply.new(reply)}
   end
   
   def initialize(options)
@@ -73,7 +135,39 @@ class Reply
     @body_text = options['body_text']
   end
   
+  def child_replies
+    replies = QuestionDatabase.instance.execute(<<-SQL, id)
+      SELECT * 
+      FROM replies
+      WHERE parent_reply = ?
+    SQL
+    Reply.new(replies.first)
+  end
+  
 end
 
 class QuestionLike
+  
+  def self.find_by_id(question_id)
+    liker_ids = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT user_liking
+      FROM question_likes
+      WHERE question_liked = ?
+    SQL
+    users = []
+    liker_ids.each do |liker_id|
+      users << User.find_by_id(liker_id.values.last)
+    end
+    users
+  end
+  
+  
+  
+end
+
+
+
+if $PROGRAM_NAME == __FILE__
+  # snarky,_user = User.new('fname' => 'Tony', 'lname' => 'Stark')
+  
 end
